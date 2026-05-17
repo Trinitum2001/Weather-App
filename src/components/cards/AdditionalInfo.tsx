4b224c4878b88fefde2bf858993c345629236700
Ruben Quintero <qrubens2001@gmail.com>
2026-05-16 20:02:28 -0600

    fix typo in wind.svg import and clean up project structure

--- src/components/cards/AdditionalInfo.tsx
+++ src/components/cards/AdditionalInfo.tsx
@@ -1,6 +1,14 @@
 import { useSuspenseQuery } from "@tanstack/react-query";
 import Card from "./card";
 import { getWeather } from "../../api";
+import Sunrise from "/src/assets/sunrise.svg?react";
+import Sunset from "/src/assets/sunset.svg?react";
+import Cloud from "/src/assets/cloud.svg?react";
+import Uv from "/src/assets/uv.svg?react";
+import Wind from "/src/assets/wind.svg?react";
+import Pressure from "/src/assets/pressure.svg?react";
+import UpArrow from "/src/assets/uparrow.svg?react";
+import { transform } from "zod";
 
 type Props = {};
 
@@ -12,9 +20,12 @@ export default function AdditionalInfo({}: Props) {
 
   return (
     <Card title="Additional Info" childrenClassName="flex flex-col gap-8">
-      {rows.map(({ label, value }) => (
+      {rows.map(({ label, value, Icon }) => (
         <div className="flex justify-between" key={value}>
-          <span className="text-gray-500">{label}</span>
+          <div className="flex gap-4">
+            <span className="text-gray-500">{label}</span>
+            <Icon className="size-8 invert" />
+          </div>
           <span>
             <FormatComponent value={value} number={data.current[value]} />
           </span>
@@ -32,6 +43,14 @@ function FormatComponent({ value, number }: { value: string; number: number }) {
       hour12: true,
     });
 
+  if (value === "wind_deg")
+    return (
+      <UpArrow
+        className="size-8 invert"
+        style={{ transform: `rotate(${number}deg)` }}
+      />
+    );
+
   return number;
 }
 
@@ -39,25 +58,31 @@ const rows = [
   {
     label: "Cloudiness (%)",
     value: "clouds",
+    Icon: Cloud,
   },
   {
     label: "UV Index",
     value: "uvi",
+    Icon: Uv,
   },
   {
     label: "Pressure (hPa)",
     value: "pressure",
+    Icon: Pressure,
   },
   {
     label: "Wind Direction (°)",
     value: "wind_deg",
+    Icon: Wind,
   },
   {
     label: "Sunrise",
     value: "sunrise",
+    Icon: Sunrise,
   },
   {
     label: "Sunset",
     value: "sunset",
+    Icon: Sunset,
   },
 ] as const;
